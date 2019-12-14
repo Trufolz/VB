8cc9a1fb295ddda061e4cba895f663dd0a9bb10c
unknown <dubielgr@gmail.com>
2019-12-14 14:23:38 +0100

    Init VB repo

---
+++ TKLab/TKLab/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If CheckBox1.Checked Then
+            While (ListBox1.Items.Contains(TextBox1.Text))
+                ListBox1.Items.Remove(TextBox1.Text)
+            End While
+        Else
+            If (TextBox1.Text <> String.Empty) Then
+                ListBox1.Items.Add(TextBox1.Text)
+            End If
+        End If
+    End Sub
+End Class
