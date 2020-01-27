e194e8d11afa872ac2b67635dd10521882759bfc
Dubson <dubielgr@gmail.com>
2020-01-27 23:39:05 +0100

    Filters done

---
+++ TKLab/TKLab/Models/Car.vb
@@ -0,0 +1,85 @@
+﻿Public Class Car
+    Public Brand As Brand
+    Public Model As String
+    Public Year As Integer
+    Public Color As Color
+    Public HorsePower As Integer
+    Public EngineVolume As Integer
+    Public Fuel As Fuel
+    Public Price As Integer
+    Public Rating As Integer
+
+End Class
+
+Public Enum Fuel
+    Diesel
+    Gasoline
+    Hybrid
+    Electric
+End Enum
+
+Public Enum Color
+    Black
+    White
+    Red
+    Blue
+    Silver
+    Green
+    Yellow
+    Maroon
+    Purple
+End Enum
+
+Public Enum Brand
+    Fiat
+    Volkswagen
+    BMW
+    Audi
+    Skoda
+    Seat
+    Kia
+    Hyundai
+    Toyota
+    Honda
+    Nissan
+    Peugeot
+    Renault
+    Citroen
+    Volvo
+    Ford
+End Enum
+
+Public Enum Volkswagen
+    Golf
+    Polo
+    Passat
+End Enum
+
+Public Enum Skoda
+    Fabia
+    Octavia
+    Superb
+End Enum
+
+Public Enum Audi
+    A1
+    A2
+    A3
+    A4
+    A5
+    A6
+    A7
+    A8
+    Q1
+    Q2
+    Q3
+    Q4
+    Q5
+    Q7
+End Enum
+
+Public Enum Toyota
+    Corolla
+    Auris
+    Avensis
+End Enum
